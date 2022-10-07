3862c29b1c5fd88f409db8881275ec2ddb63a664
Will <wrightw404@gmail.com>
2022-10-07 11:27:31 -0700

    created models for category product product tag and tag

--- models/Category.js
+++ models/Category.js
@@ -3,10 +3,20 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+// define columns
 Category.init(
-  {
-    // define columns
+  { 
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+    },
+    
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
